c78f1c945b7cea3196e27653be314a18c2b7946d
Henrik Wingerei <henrikwingerei@gmail.com>
2014-02-01 16:54:14 +0100

    Added missing feedservice.go from last commit

---
+++ feedservice.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/SlyMarbo/rss"
+	"errors"
+)
+
+var feed1 = &rss.Feed{UpdateURL: "http://www.vg.no/rss/create.php?categories=20&keywords=&limit=10"}
+
+var feeds = []*rss.Feed{feed1}
+
+func Add(feed *rss.Feed) error {
+	if (feedExists(feed)) {
+		return errors.New("Feed with UpdateURL already exists")
+	}
+	feeds = append(feeds, feed)
+	return nil
+}
+
+func feedExists(feed *rss.Feed) bool {
+	for _, v := range feeds {
+		if (v.UpdateURL == feed.UpdateURL) {
+			return true
+		}
+	}
+	return false
+}
+
+func GetAll() []*rss.Feed {
+	return feeds
+}
